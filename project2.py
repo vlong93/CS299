173756f90982c3fb2c0e39faf45d57f89034963f
Vivian <vlong93@gmail.com>
2018-01-17 12:24:25 -0800

    updated p2

--- project2.py
+++ project2.py
@@ -1,6 +1,7 @@
 # Author: Vivian Long
 # Assignment: Project 2
-# Completed:
+# Completed: 1/17/2018
+
 
 # Problem 1
 
@@ -23,12 +24,12 @@ elif (system == 'e') or (system == 'E'):
 
     bmi = round(weight / (height ** 2) * 703, 2)
 
-# User enters an invalid choice
+# User enters something else
 else:
     print("Please enter a valid response.")
     sys.exit(0)
 
-# Output BMI and corresponding message
+# Output BMI and appropriate corresponding message
 print("BMI:", bmi)
 
 if bmi <= 24:
@@ -106,7 +107,134 @@ BMI: 40.74
 Your BMI indicates you are extremely obese.
 
 Test 9: Error due to wrong data format
+========= RESTART: /Users/vlong/Documents/project/CS299/project2.py =========
+Would you like to use the metric or English system? Enter 'm' for metric or 'e' for English: j
+Please enter a valid response.
 
+Test 10: Error due to wrong data values
+========= RESTART: /Users/vlong/Documents/project/CS299/project2.py =========
+Would you like to use the metric or English system? Enter 'm' for metric or 'e' for English: e
+Enter weight in lbs: -10
+Enter height in inches: 2
+BMI: -1757.5
+Your BMI is normal.
+
+Test 11: Error
+========= RESTART: /Users/vlong/Documents/project/CS299/project2.py =========
+Would you like to use the metric or English system? Enter 'm' for metric or 'e' for English: e
+Enter weight in lbs: 145
+Enter height in inches: 5'7
+Traceback (most recent call last):
+  File "/Users/vlong/Documents/project/CS299/project2.py", line 23, in <module>
+    height = float(input("Enter height in inches: "))
+ValueError: could not convert string to float: "5'7"
+
+Test 12: Error
+========= RESTART: /Users/vlong/Documents/project/CS299/project2.py =========
+Would you like to use the metric or English system? Enter 'm' for metric or 'e' for English: m
+Enter weight in kg: 60
+Enter height in meters: 170
+BMI: 0.0
+Your BMI is normal.
+'''
+
+
+
+
+# Problem 2
+
+import random
+
+rps = ["Rock", "Paper", "Scissors"]
+
+# Computer randomly generates string from list
+npc = random.choice(rps)
+
+# Asks user for choice
+user = str(input("Rock, paper, or scissors? "))
+
+# User chooses rock
+if (user == "rock") or (user == "Rock"):
+    print("You chose", user)
+    print("NPC chose", npc)
+    if (npc == "Rock"):                
+        print("It's a tie")
+    elif (npc == "Paper"):
+        print("You lose")
+    elif (npc == "Scissors"):
+        print("You win!")
+
+# User chooses paper
+elif (user == "paper") or (user == "Paper"):
+    print("You chose", user)
+    print("NPC chose", npc)
+    if (npc == "Rock"):                
+        print("You win!")
+    elif (npc == "Paper"):
+        print("It's a tie")
+    elif (npc == "Scissors"):
+        print("You lose")
+
+# User chooses scissors
+elif (user == "scissors") or (user == "Scissors"):
+    print("You chose", user)
+    print("NPC chose", npc)
+    if (npc == "Rock"):
+        print("You lose")
+    elif (npc == "Paper"):
+        print("You win!")
+    elif (npc == "Scissors"):
+        print("It's a tie")
+
+# User enters something else
+else:
+    print("Please enter a valid response")
+    
+'''
+Output 1
+========= RESTART: /Users/vlong/Documents/project/CS299/project2.py =========
+Rock, paper, or scissors? rock
+You chose rock
+NPC chose Rock
+It's a tie
+
+Output 2
+========= RESTART: /Users/vlong/Documents/project/CS299/project2.py =========
+Rock, paper, or scissors? paper
+You chose paper
+NPC chose Scissors
+You lose
+
+Output 3
+========= RESTART: /Users/vlong/Documents/project/CS299/project2.py =========
+Rock, paper, or scissors? scissors
+You chose scissors
+NPC chose Paper
+You win!
+
+Output 4
+========= RESTART: /Users/vlong/Documents/project/CS299/project2.py =========
+Rock, paper, or scissors? Paper
+You chose Paper
+NPC chose Paper
+It's a tie
+
+Output 5
+========= RESTART: /Users/vlong/Documents/project/CS299/project2.py =========
+Rock, paper, or scissors? Rock
+You chose Rock
+NPC chose Scissors
+You win!
+
+Output 6
+========= RESTART: /Users/vlong/Documents/project/CS299/project2.py =========
+Rock, paper, or scissors? hello
+Please enter a valid response
+
+Output 7
+========= RESTART: /Users/vlong/Documents/project/CS299/project2.py =========
+Rock, paper, or scissors? 1234
+Please enter a valid response
 
 
 '''
